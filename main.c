53638c1b78f6a6d8b6a55346fca200d4f9a474ed
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2025-10-29 11:39:47 +0100

    two Rainbowy triangle

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+
 #define VERTEX_SHADER_PATH "shader/vertex_shader.glsl"
 #define FRAG_SHADER_PATH "shader/fragment_shader.glsl"
 
@@ -62,11 +65,17 @@ int main(void) {
 
 
 
-    GLfloat vertices[] = {
-        //positions             //colors
-        -0.5f, -0.5f,  0.0f,    1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.0f,    0.0f,  1.0f,  0.0f,
-         0.5f, -0.5f,  0.0f,    0.0f,  0.0f,  1.0f
+    float vertices[] = {
+        // positions            // colors              // texture coords
+         0.5f,  0.5f,  0.0f,    1.0f,  0.0f,  0.0f,    1.0f,  1.0f, // top right
+        -0.5f,  0.5f,  0.0f,    0.0f,  1.0f,  0.0f,    0.0f,  1.0f, // top left 
+        -0.5f, -0.5f,  0.0f,    0.0f,  0.0f,  1.0f,    0.0f,  0.0f, // bottom left
+         0.5f, -0.5f,  0.0f,    0.0f,  0.0f,  0.0f,    1.0f,  0.0f  // bottom right
+    };
+
+    unsigned int indices[] = {
+        0,1,3,  //first triangle
+        1,2,3   //second triangle
     };
 
     // -------------------- Compile and Link shaders (Vertex and Fragment) into a Shader Program --------------------
@@ -87,27 +96,56 @@ int main(void) {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // -------------------- Generating a Texture --------------------
+    unsigned int texture;
+    glGenTextures(1, &texture);  
+    glBindTexture(GL_TEXTURE_2D, texture);  
 
-    
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0); 
+    if(!data) {
+        fprintf(stderr,"Failed to load Texture");
+        return -1;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+
     // -------------------- Create VBO and VAO --------------------
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);            // Bind it as the current array buffer
     
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copy data to GPU 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
+
+    
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    //Clean up
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0);
 
     // -------------------- Render Loop --------------------
@@ -119,7 +157,7 @@ int main(void) {
         glUseProgram(shaderProgram);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
