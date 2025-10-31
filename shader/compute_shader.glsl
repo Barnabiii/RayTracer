8b99d313ab7723044b33579bba9b1addc1682dbb
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2025-10-31 12:33:49 +0100

    colors

--- shader/compute_shader.glsl
+++ shader/compute_shader.glsl
@@ -11,7 +11,7 @@ void main() {
     c.x = (3.5*UVs.x) - 2.5;
     c.y = (3.0*UVs.y) - 1.5;
 
-    vec3 px = vec3(1.0);
+    vec3 px = vec3(0.0);
     float depth = 10;
     vec2 z = vec2(0.0);
 
@@ -20,8 +20,7 @@ void main() {
         z = vec2(pow(z.x,2.0) - pow(z.y,2.0),(2.0*z.x*z.y)) + c;
         if (length(z) > 2.0) {
             float s = float(i)/depth;
-            px = vec3(s);
-            //px = vec3((cos(s*3.14)+1)*0.3, s, 1.5  -s);
+            px = vec3((cos(s*3.14)+1)*0.3, s, 1.5  -s);
             break;
         }
     }
