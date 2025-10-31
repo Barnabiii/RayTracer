d359c28318dc82df225880150d90ee9a9d0fb323
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2025-10-31 12:33:07 +0100

    perfect greyscale

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <windows.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
--- shader/compute_shader.glsl
+++ shader/compute_shader.glsl
@@ -8,19 +8,20 @@ void main() {
     vec2 UVs = vec2(gl_GlobalInvocationID.xy)/(gl_NumWorkGroups.xy*gl_WorkGroupSize.xy);
 
     vec2 c;
-    c.x = (4.0*UVs.x) - 2.5;
+    c.x = (3.5*UVs.x) - 2.5;
     c.y = (3.0*UVs.y) - 1.5;
 
-    vec3 px = vec3(0.0);
+    vec3 px = vec3(1.0);
+    float depth = 10;
+    vec2 z = vec2(0.0);
 
-    int depth = 10;
-    vec2 z = vec2(0);
-
-    for (int i = 1; i <= depth; i+=1) {
-        z.x = pow(z.x,2.0)-pow(z.y,2.0)+c.x;
-        z.y = 2.0 * z.x *z.y +c.y;
+    int i;
+    for (i = 0; i <= int(depth); i++) {
+        z = vec2(pow(z.x,2.0) - pow(z.y,2.0),(2.0*z.x*z.y)) + c;
         if (length(z) > 2.0) {
-            px = vec3(float(i)/float(depth));
+            float s = float(i)/depth;
+            px = vec3(s);
+            //px = vec3((cos(s*3.14)+1)*0.3, s, 1.5  -s);
             break;
         }
     }
