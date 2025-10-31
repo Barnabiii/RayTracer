72d7a5e1dacd7b6900cc86bf34760b614175fd69
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2025-10-31 11:32:48 +0100

    weird fractal

--- shader/compute_shader.glsl
+++ shader/compute_shader.glsl
@@ -4,8 +4,24 @@ layout(rgba32f, binding = 0) uniform image2D screen;
 
 void main() {
     ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);
-    vec2 st = vec2(gl_GlobalInvocationID.xy)/(vec2(gl_NumWorkGroups.xy)*vec2(8,4));
-    vec4 pxColor = vec4(st, 0.0, 1.0);
+    vec2 UVs = vec2(gl_GlobalInvocationID.xy)/vec2(gl_NumWorkGroups.x*8.0,gl_NumWorkGroups.y*4.0);
+
+    vec2 c;
+    c.x = (4.0*UVs.x) - 2.5;
+    c.y = (3.0*UVs.y) - 1.5;
+
+    vec3 px = vec3(0.0);
+
+    int depth = 5;
+    vec2 z = vec2(0);
+
+    for (int i = 1; i <= depth; i+=1) {
+        z = z*z + c;
+        if (length(z) > 2) {
+            px = vec3(i/depth);
+            break;
+        }
+    }
     
-    imageStore(screen,pixelCoords,pxColor);
+    imageStore(screen,pixelCoords,vec4(px,1.0));
 }
